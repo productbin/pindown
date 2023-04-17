5d0596178af7a03e72e0b637ca4ec431dcf933ea
Mishalabdullah <github.ijvob@simplelogin.co>
2023-04-17 14:40:41 +0530

    basic navbar layout done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    borderRadius: {
+      lg: "1rem",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gra-one": "#0E1C26",
+        "gra-two": "#2A454B",
+        "gra-tri": "#294861",
       },
     },
   },
   plugins: [],
-}
+};
