9e243f2b293acbb1694b423b639422c7989b70e8
Mishalabdullah <github.ijvob@simplelogin.co>
2023-04-20 17:11:54 +0530

    first page done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+
 export default function Home() {
   return (
-    <main className=" justify-center text-white ">
-      <div>
-        <h1  className="border-white border-50 border-solid justify-center">Hello World</h1>
+    <main className=" justify-center text-white min-h-[1000px]">
+      <div className="flex border-white border-50 border-solid  items-center justify-center mt-16 m-2">
+        <h1 className="font-bold text-7xl">Powerful for developers.</h1>
+      </div>
+      <div className="flex border-white border-50 border-solid  items-center justify-center m-1">
+        <h1 className="font-bold text-5xl">Secure for everyone.</h1>
+      </div>
+      <div className="flex items-center justify-center mt-16">
+        <div>
+          <button className="bg-white text-gra-two font-bold p-3 m-2 rounded-lg  hover:bg-gray-700 hover:text-white  ">
+            START HERE{" "}
+          </button>
+        </div>
+        <div>
+          <button className="font-bold rounded-lg border-white border-2 p-3 w-28 m-2  hover:bg-white hover:text-gray-700 ">
+            DOCS
+          </button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
