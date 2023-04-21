050ea0ff8412b2f2a38ba1e188dad579229060b6
Mishalabdullah <github.ijvob@simplelogin.co>
2023-04-21 11:50:17 +0530

    login page added

---
+++ app/login/page.js
@@ -0,0 +1,13 @@
+import "../globals.css";    
+export default function login(){
+  return(
+    <div className="flex justify-around">
+      <div>
+        <img src="group1.png" alt="not found"/>
+      </div>
+      <div>
+        <img src="group2.png" alt="not found"/>
+      </div>
+    </div>
+  );
+}
