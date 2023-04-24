17bc665afacfae9d9b6c33ce1a6757da5cc7c912
Mishalabdullah <github.ijvob@simplelogin.co>
2023-04-24 10:45:12 +0530

    res

--- app/footer.js
+++ app/footer.js
@@ -9,18 +9,18 @@ import {
 import { redirect } from "next/dist/server/api-utils";
 function Footer() {
   return (
-    <div className="flex border-4 border-white rounded-lg m-4 mt-32 ">
-      <div className=" max-w-[45%]">
-        <div className="text-white font-bold m-10 text-4xl">
+    <div className="sm:flex border-4 border-white rounded-lg m-4 mt-32 ">
+      <div className=" sm:max-w-[45%]">
+        <div className="text-white font-bold m-10 text-2xl sm:text-4xl">
           <h1>STAY SYNCED</h1>
         </div>
-        <div className="text-white  text-xl m-10 font-medium ">
+        <div className="text-white  text-sm sm:text-xl m-10 font-medium ">
           <p>
             Join our mailing list to stay in sync with our newest feature
             releases, NFT drops, and tips and tricks for navigating Pindown.
           </p>
         </div>
-        <div className="flex justify-around">
+        <div className="sm:flex sm:justify-around">
           <div className=" ml-10 mt-10">
             <input
               className="p-5 rounded-lg"
@@ -29,14 +29,14 @@ function Footer() {
               name="email"
             />
           </div>
-          <div className="ml-2 mb-10 mt-10 mr-10">
-            <button className="rounded-lg text-white font-semibold border-4 border-white p-4">
+          <div className=" flex ml-2 mb-10 mt-10  sm:mr-10">
+            <button className="rounded-lg text-white font-semibold border-4 justify-items-center  border-white p-4">
               Subscribe
             </button>
           </div>
         </div>
       </div>
-      <div className="min-w-[25%] ml-64">
+      <div className="sm:min-w-[25%] sm:ml-64">
         <div className="text-white font-bold m-10 text-4xl">
           <h1>JOIN OUR TRIBE!!</h1>
         </div>
