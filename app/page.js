2a5404772dfac2d6714b36fd3ea53f502333357d
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-04 21:23:16 +0530

    styling landing page

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Montserrat } from "@next/font/google";
+import { Montserrat } from '@next/font/google';
 export default function Home() {
   return (
     <main className=" justify-center text-white">
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="flex items-center justify-center mt-16">
         <div>
           <button className="bg-white text-gra-two font-bold p-3 m-2 rounded-lg  hover:bg-gray-700 hover:text-white  ">
-            START HERE{" "}
+            START HERE{' '}
           </button>
         </div>
         <div>
@@ -26,19 +26,74 @@ export default function Home() {
       </div>
       <div className="mb-8">
         <div className=" flex justify-center items-center m-5 mt-40 ">
-          <h1 className="font-bold  text-2xl sm:text-5xl items-center">
-            WHY PINDOWN ?
-          </h1>
+          <div className="">
+            <div className="flex justify-center m-5">
+              {' '}
+              <h1 className="font-bold text-2xl sm:text-5xl items-center">
+                WHY PINDOWN ?
+              </h1>
+            </div>
+            <div>
+              {' '}
+              <h1 className="font-bold  text-xl sm:text-4xl items-center">
+                Pindown is a blockchain-based document verifier.{' '}
+              </h1>
+            </div>
+          </div>
         </div>
-        <div className="sm:flex items-center sm:justify-around">
-          <div className=" p-10 sm:p-4 m-2">
-            <img src="whycard2.png" alt="Girl in a jacket" />
+        <div className="items-center sm:justify-around">
+          <div className="sm:flex justify-evenly m-10">
+            <div className=" min-w-[25%]  p-10 sm:p-4 m-2">
+              <img src="transparent.png" alt="Girl in a jacket" />
+            </div>
+            <div className=" m-8 p-10 text-center justify-center items-center self-center ">
+              <p>
+                Anyone can view the legit source and destination of any
+                certificate(or document in general). It records the document
+                transfer data and link details on-chain such that only the
+                issuer can specify the link to the document and receiver address
+                and the source(issuer public key) is signed by the issuer.
+                Anyone can also easily verify the same details through the
+                PinDown Dapp.
+              </p>
+            </div>
           </div>
-          <div className=" p-10 sm:p-4 m-2">
-            <img src="whycard2.png" alt="not found" />
-          </div>
-          <div className="p-10 sm:p-4 m-2">
-            <img src="whycard2.png" alt="not found" />
+          <div className="items-center sm:justify-around">
+            <div className="sm:flex justify-evenly m-10">
+              <div className=" m-8 p-10 text-center justify-center items-center self-center ">
+                <p>
+                  Anyone can view the legit source and destination of any
+                  certificate(or document in general). It records the document
+                  transfer data and link details on-chain such that only the
+                  issuer can specify the link to the document and receiver
+                  address and the source(issuer public key) is signed by the
+                  issuer. Anyone can also easily verify the same details through
+                  the PinDown Dapp.
+                </p>
+              </div>
+
+              <div className=" min-w-[25%]  p-10 sm:p-4 m-2">
+                <img src="transparent.png" alt="Girl in a jacket" />
+              </div>
+            </div>
+            <div className="items-center sm:justify-around">
+              <div className="sm:flex justify-evenly m-10">
+                <div className=" min-w-[25%]  p-10 sm:p-4 m-2">
+                  <img src="transparent.png" alt="Girl in a jacket" />
+                </div>
+                <div className=" m-8 p-10 text-center justify-center items-center self-center ">
+                  <p>
+                    Anyone can view the legit source and destination of any
+                    certificate(or document in general). It records the document
+                    transfer data and link details on-chain such that only the
+                    issuer can specify the link to the document and receiver
+                    address and the source(issuer public key) is signed by the
+                    issuer. Anyone can also easily verify the same details
+                    through the PinDown Dapp.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
