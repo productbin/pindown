c58daf67c34dc21ee2e79b9f8bc4e8032265112a
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-09 20:04:54 +0530

    redesign

--- app/upload/page.js
+++ app/upload/page.js
@@ -53,12 +53,10 @@ export default function Upload() {
     const imageurl = document.getElementById('imgurl').value;
     const description = document.getElementById('desc').value;
     const attributes = document.getElementById('attribute').value;
-    const nofnft = document.getElementById('nft').value;
-    let inputData = {
+       let inputData = {
       Image_Url: imageurl,
       Description: description,
       Attri: attributes,
-      nftno: nofnft,
     };
     const blob = new Blob([JSON.stringify(inputData)], {
       type: 'application/json',
@@ -68,54 +66,49 @@ export default function Upload() {
   }
   return (
     <div className="m-8 border-4 border-white rounded-lg opacity-90 p-5 text-white">
-      <div className="sm:flex sm:justify-evenly rounded-lg 	">
+      <div className="sm:flex sm:justify-center rounded-lg 	">
         <div>
-          <div className="m-5">
-            <h1 className="font-semibold text-white p-2  m-1">Upload Image</h1>
-            <input
-              id="imgurl"
-              className="rounded-lg p-2 border-4 bg-transparent border-white"
-              type="file"
-              autoFocus
-            />
-          </div>
-          <div className="m-5">
-            <h1 className="font-semibold text-white p-2 m-1">Attributes</h1>
-            <input
-              id="attribute"
-              className="bg-transparent border-4 border-white rounded-lg p-2 "
-              type="text"
-            />
-          </div>
-          <div className="m-5">
-            <h1 className="font-semibold text-white p-2 m-1">
-              Number Of NFT's
-            </h1>
-            <input
-              id="nft"
-              className="rounded-lg p-2 bg-transparent border-white border-4"
-              type="text"
-            />
-          </div>
-        </div>
-        <div>
-          <input
-            type="file"
-            id="upfile"
-            accept=".csv"
-            className="space-x-8 bg-transperent p-3 border-4 border-white text-white sm:ml-8  font-semibold  mt-10 rounded-lg"
-            hidden
-          />
-        </div>
-
-        <div>
-          <div className="m-5 rounded-lg  truncate break-words overflow-x-hidden  ">
-            <h1 className="font-semibold text-white p-2 m-1">Desciption</h1>
-            <input
-              id="desc"
-              className="rounded-lg bg-transparent  object-fill  border-4 border-white h-64"
-              type="text"
-            />
+          <div class="flex w-72 flex-col items:center  m-5 items-end gap-6">
+            <div class="relative h-10 m-5  w-full min-w-[24rem]">
+              <input
+                id="attribute"
+                class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent  py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+              />
+              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Attributes
+              </label>
+            </div>
+            <div class="relative h-11 m-5 w-full min-w-[24rem]">
+              <input
+                id="desc"
+                class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+              />
+              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11p      nftno: nofnft,
+x] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Description
+              </label>
+            </div>
+            <div class="relative flex h-10 w-full m-5 min-w-[24rem] max-w-[24rem]">
+              <input
+                id="imgurl"
+                type="file"
+                class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+                required
+              />
+              <button
+                class="!absolute right-1 top-1 z-10 select-none rounded bg-pink-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
+                type="button"
+                data-ripple-light="true"
+              >
+                Send
+              </button>
+              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Upload Photo
+              </label>
+            </div>
           </div>
         </div>
       </div>
