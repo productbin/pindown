3727679f5ebf3beb20991ce4a7a2f9ea9fbc50fe
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-10 09:50:10 +0530

    imagetoIpfs

--- app/upload/page.js
+++ app/upload/page.js
@@ -7,10 +7,20 @@ import Papa from 'papaparse';
 
 export var ipfsURL;
 export var theData = [];
-
+var theImageUrl;
 export default function Upload() {
   function getAccessToken() {
     return process.env.NEXT_PUBLIC_API_KEY;
+  }
+
+  async function imageStorage() {
+    const imageurl = document.getElementById('imgurl').value;
+    const imageData = [new File([imageurl], 'image.png')];
+    const client = new Web3Storage({ token: getAccessToken() });
+    const rootCid = await client.put(imageData);
+    ipfsURL = 'https://dweb.link/ipfs/' + rootCid;
+    console.log(ipfsURL);
+    theImageUrl = ipfsURL;
   }
   async function webstorage(data) {
     const client = new Web3Storage({ token: getAccessToken() });
@@ -50,11 +60,10 @@ export default function Upload() {
   }
 
   function getdata() {
-    const imageurl = document.getElementById('imgurl').value;
     const description = document.getElementById('desc').value;
     const attributes = document.getElementById('attribute').value;
-       let inputData = {
-      Image_Url: imageurl,
+    let inputData = {
+      Image_Url: theImageUrl,
       Description: description,
       Attri: attributes,
     };
@@ -85,8 +94,10 @@ export default function Upload() {
                 class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
               />
-              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11p      nftno: nofnft,
-x] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label
+                class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11p      nftno: nofnft,
+x] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+              >
                 Description
               </label>
             </div>
@@ -123,6 +134,18 @@ x] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-
             Upload Wallet Address
           </button>
         </div>
+
+        <div>
+          {' '}
+          <button
+            id="upbtn"
+            onClick={imageStorage}
+            className="bg-white p-3 sm:ml-10 font-semibold  mt-10 rounded-lg"
+          >
+            Upload Photo To IPFS
+          </button>
+        </div>
+
         <div>
           {' '}
           <button
