95d8ac84342696e3edd7c1819eb773e4ef9bd479
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-10 17:36:39 +0530

    button redesign

--- app/components/Cwalllet.js
+++ app/components/Cwalllet.js
@@ -3,20 +3,55 @@ import { useConnect } from 'wagmi';
 function Profile() {
   const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect();
- return (
+  return (
     <div>
       {connectors.map((connector) => (
         <button
-          className="bg-white p-2 rounded-lg  hover:bg-gray-700 hover:text-white"
           disabled={!connector.ready}
           key={connector.id}
           onClick={() => connect({ connector })}
+          className=" rounded-lg relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
         >
-          {!connector.ready && ' Connect Wallet'}
-          {connector.ready && "Connected"}
-          {isLoading &&
-            connector.id === pendingConnector?.id &&
-            ' (Connecting)'}
+          <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+          <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg
+              className="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg
+              className="w-5 h-5 text-green-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+            {!connector.ready && ' Connect Wallet'}
+            {connector.ready && 'Connected'}
+            {isLoading &&
+              connector.id === pendingConnector?.id &&
+              ' (Connecting)'}
+          </span>
         </button>
       ))}
 
