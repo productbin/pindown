b321d7981a767ce761a4b43300c027251d832b05
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-12 15:53:49 +0530

    csv parsing added

--- app/upload/page.js
+++ app/upload/page.js
@@ -55,7 +55,7 @@ async function Everything() {
   }
 
   function startPapa() {
-    const theFile = Papa.parse(document.getElementById('upfile').files[0], {
+    const theFile = Papa.parse(document.getElementById('csvfile').files[0], {
       download: true,
       header: false,
       skipEmptyLines: true,
@@ -67,13 +67,9 @@ async function Everything() {
   }
 
   function parsecsv() {
-    const fileInput = document.getElementById('upfile');
-    fileInput.click();
+    const fileInput = document.getElementById('csvfile');
 
-    fileInput.addEventListener('change', () => {
-      const uploadedFile = fileInput.files[0];
-      startPapa();
-    });
+       startPapa();
   }
 
   function getdata() {
@@ -115,30 +111,21 @@ async function Everything() {
                 Description
               </label>
             </div>
-            <div className="relative h-10 m-5  w-full min-w-[24rem]">
+            <div className="relative flex h-10 w-full m-5 min-w-[24rem] max-w-[24rem]">
               <input
+                id="csvfile"
                 type="file"
                 accept=".csv"
-                id="attribute"
-                className="peer h-full w-full rounded-[7px] border-2eborder-blue-gray-200 border-t-transparent bg-transparent  py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-4 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                placeholder=" "
                 onChange={parsecsv}
+                className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+                required
+
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                Upload CSV File
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Upload Wallet Addresses
               </label>
             </div>
-
-            <div>
-              <input
-                type="file"
-                id="upfile"
-                accept=".csv"
-                className="space-x-8 bg-transperent p-3 border-4 border-white text-white sm:ml-8  font-semibold  mt-10 rounded-lg"
-                hidden
-              />
-            </div>
-
             <div className="relative flex h-10 w-full m-5 min-w-[24rem] max-w-[24rem]">
               <input
                 id="imgurl"
@@ -159,24 +146,6 @@ async function Everything() {
               </label>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="m-5  sm:flex sm:justify-evenly text-gra-one ">
-        <div className="min-w-1/4">
-          <button
-            onClick={parsecsv}
-            className="border-none relative inline-block text-lg group"
-          >
-            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-              <span className="absolute left-0 w-64 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  group-hover:-rotate-180 ease"></span>
-              <span className="relative">Upload Wallet Address</span>
-            </span>
-            <span
-              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  rounded-lg group-hover:mb-0 group-hover:mr-0"
-              data-rounded="rounded-lg"
-            ></span>
-          </button>
         </div>
         <div className="min-w-1/4">
           {' '}
