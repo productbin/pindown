dc62c6269b6cc311278db8d45a3c7388a2d749cf
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-22 21:54:49 +0530

    try

--- app/upload/page.js
+++ app/upload/page.js
@@ -46,10 +46,38 @@ const client = createClient({
 const MyComponent = () => {
   const { write } = useContractWrite(config);
 
+<<<<<<< HEAD
   return <WagmiConfig>{write}</WagmiConfig>;
 };
 //Function For Minting NFT
 // Called Inside the Upload Function Bottom
+=======
+function Mint() {
+  const config = {
+    address: "0x41E405438dF59D438D62385e762B7e4B54AE2517",
+    abi: abiData,
+    functionName: "bulkMint",
+    args: [ipfsURL, theData],
+  };
+
+  const { write } = useContractWrite(config);
+  return (
+    <div className="flex justify-center">
+      <button onClick={write} className="relative inline-block text-lg group">
+        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+          <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+          <span className="absolute left-0 w-64 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  group-hover:-rotate-180 ease"></span>
+          <span className="relative">Transfer The NFT's</span>
+        </span>
+        <span
+          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  rounded-lg group-hover:mb-0 group-hover:mr-0"
+          data-rounded="rounded-lg"
+        ></span>
+      </button>{" "}
+    </div>
+  );
+}
+>>>>>>> parent of b7e8cdb (cleaning)
 
 export default function Upload() {
   const [uploading, setUploading] = useState(false);
