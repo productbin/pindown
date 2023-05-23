dffca6123509d51b2de1aa0cb178ce11f88b5b79
Mishalabdullah <github.ijvob@simplelogin.co>
2023-05-23 06:35:54 +0530

    rollback

--- app/upload/page.js
+++ app/upload/page.js
@@ -1,6 +1,6 @@
 "use client";
 import "../globals.css";
-import ArrayShow from "../components/displayWallet";
+import ArrayDisplay from "../components/displayWallet";
 import { Web3Storage } from "web3.storage";
 import { File } from "web3.storage";
 import Papa from "papaparse";
@@ -37,9 +37,6 @@ const client = createClient({
   webSocketProvider,
 });
 
-//Function For Minting NFT
-// Called Inside the Upload Function Bottom
-
 function Mint() {
   const config = {
     address: "0x41E405438dF59D438D62385e762B7e4B54AE2517",
@@ -71,19 +68,10 @@ export default function Upload() {
   const [displayWallets, setDisplayWallets] = useState(false);
   const [isButtonHovered, setIsButtonHovered] = useState(false);
   const [uploadedUrl, setUploadedUrl] = useState("");
-  const [displayMonoWallets, setDisplayMonoWallets] = useState(false);
-  const [theData, setTheData] = useState([]);
   function getAccessToken() {
     return process.env.NEXT_PUBLIC_API_KEY;
   }
 
-  const monoInput = () => {
-    const data = document.getElementById("monoWalletAddress").value;
-    setTheData((prevData) => [...prevData, data]); // Update theData array with new value
-    setDisplayMonoWallets(true);
-    alert(data + " Wallet Address Added");
-    document.getElementById("monoWalletAddress").value = ""; // Set the value to an empty string
-  };
   async function Everything() {
     try {
       setUploading(true);
@@ -95,29 +83,6 @@ export default function Upload() {
     } finally {
       setUploading(false);
     }
-  }
-
-  function handleImageSelect() {
-    const fileInput = document.getElementById("imgurl");
-    const file = event.target.files[0];
-
-    if (!file) {
-      console.error("No file selected");
-      return;
-    }
-
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const imagePreview = document.getElementById("imagePreview");
-      const img = document.createElement("img");
-      img.src = e.target.result;
-      img.alt = "Selected Image";
-      imagePreview.innerHTML = "";
-      imagePreview.appendChild(img);
-    };
-
-    reader.readAsDataURL(file);
   }
 
   async function imageStorage() {
@@ -178,7 +143,7 @@ export default function Upload() {
       description: Description,
       image: theImageUrl,
       name: Name,
-      attributes: [Attributes],
+      attributes: Attributes,
     };
     const blob = new Blob([JSON.stringify(inputData)], {
       type: "application/json",
@@ -188,11 +153,11 @@ export default function Upload() {
   }
   return (
     <WagmiConfig client={client}>
-      <div className="rounded-lg p-5 w-full text-white">
-        <div className="rounded-lg bg-white bg-opacity-5 justify-evenly">
-          <div className="sm:flex sm:justify-center  ">
+      <div className="rounded-lg p-5 text-white">
+        <div className="flex justify-evenly">
+          <div className="sm:flex sm:justify-center  rounded-lg bg-white bg-opacity-5 ">
             <div>
-              <div className="flex flex-col items-center  space-y-12  m-5 items-end gap-6">
+              <div className="flex-col items-center  space-y-12  m-5 items-end gap-6">
                 <div className="relative flex m-5 h-10 w-full m-5 min-w-[24rem] max-w-[24rem]">
                   <input
                     id="name"
@@ -223,80 +188,44 @@ export default function Upload() {
                     Description{" "}
                   </label>
                 </div>
-                <div className="items-center  flex-col">
-                  <div>
-                    {(displayWallets || displayMonoWallets) && (
-                      <ArrayShow
-                        onChange={parsecsv || displayMonoWallets}
-                        array={theData}
+                <div className="relative flex h-10 m-5 w-full m-5 min-w-[24rem] max-w-[24rem]">
+                  <label
+                    htmlFor="csvfile"
+                    className="relative inline-block px-4 py-2 h-full w-full text-sm font-medium leading-5 text-white border rounded-[7px] cursor-pointer bg-transparent hover:border-pink-500 hover:text-pink-500"
+                  >
+                    <div className="flex justify-between">
+                      <span className="">Choose CSV File</span>
+                      <input
+                        id="csvfile"
+                        type="file"
+                        accept=".csv"
+                        onChange={parsecsv}
+                        className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                        placeholder=""
+                        required
                       />
-                    )}
-                  </div>
-                </div>
-                <div className="flex  items-center m-0">
-                  <div className="relative flex h-10 m-5 w-full m-5 min-w-[24rem] max-w-[24rem]">
-                    <label
-                      htmlFor="csvfile"
-                      className="relative inline-block px-4 py-2 h-full w-full text-sm font-medium leading-5 text-white border rounded-[7px] cursor-pointer bg-transparent hover:border-pink-500 hover:text-pink-500"
-                    >
-                      <div className="flex justify-between">
-                        <span className="">Choose CSV File</span>
-                        <input
-                          id="csvfile"
-                          type="file"
-                          accept=".csv"
-                          onChange={parsecsv}
-                          className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-                          placeholder=""
-                          required
-                        />
-                        <button
-                          onMouseEnter={() => setIsButtonHovered(true)}
-                          onMouseLeave={() => setIsButtonHovered(false)}
-                          className="relative"
-                        >
-                          <div>
-                            <FontAwesomeIcon
-                              icon={faInfoCircle}
-                              style={{ color: "white" }}
-                            />
-                          </div>
-                          {isButtonHovered && (
-                            <img
-                              className="absolute top-[-25rem] left-1/2 min-w-[500px] h-[500px] rounded-full object-cover transition-transform transform hover:scale-125"
-                              src="walletI.png"
-                              alt="Image"
-                            />
-                          )}
-                        </button>
-                      </div>
-                    </label>
-                  </div>
-                  <div className="items-center m-5 font-bold text-3xl">
-                    {"OR"}
-                  </div>
-                  <div className="relative flex m-5 h-10 w-full m-5 min-w-[24rem] max-w-[24rem]">
-                    <input
-                      id="monoWalletAddress"
-                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-2 border-l-2 border-r-2 bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline-none transition-all placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-pink-500 focus:border-t-transparent focus:outline-none disabled:border-0 disabled:bg-blue-gray-50"
-                      placeholder=" "
-                    />
-                    <button
-                      onClick={monoInput}
-                      className="!absolute right-1 top-1 z-10 select-none rounded bg-pink-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
-                      data-ripple-light="false"
-                    >
-                      ADD
-                    </button>
-
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                      Insert Wallet Address{" "}
-                    </label>
-                  </div>
-                </div>
-                <div className="items-center  w-64 m-0 flex-col">
-                  <div id="imagePreview"></div>
-                </div>
+                      <button
+                        onMouseEnter={() => setIsButtonHovered(true)}
+                        onMouseLeave={() => setIsButtonHovered(false)}
+                        className="relative"
+                      >
+                        <div>
+                          <FontAwesomeIcon
+                            icon={faInfoCircle}
+                            style={{ color: "white" }}
+                          />
+                        </div>
+                        {isButtonHovered && (
+                          <img
+                            className="absolute top-[-25rem] left-1/2 min-w-[500px] h-[500px] rounded-full object-cover transition-transform transform hover:scale-125"
+                            src="walletI.png"
+                            alt="Image"
+                          />
+                        )}
+                      </button>
+                    </div>
+                  </label>
+                </div>{" "}
                 <div className="relative  flex  h-10 m-5 w-full m-5 min-w-[24rem] max-w-[24rem]">
                   <input
                     id="imgurl"
@@ -304,7 +233,6 @@ export default function Upload() {
                     className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     placeholder=" "
                     required
-                    onChange={handleImageSelect}
                   />
                   <button
                     onClick={Everything}
@@ -321,7 +249,15 @@ export default function Upload() {
               </div>
             </div>
           </div>
-          <div></div>
+          <div>
+            <div className="items-center flex-col">
+              <div className="">
+                {displayWallets && (
+                  <ArrayDisplay onChange={parsecsv} array={theData} />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </WagmiConfig>
