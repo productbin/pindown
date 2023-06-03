d23e30fccc70bc3372a4b2f8fdd21478539b1bdb
MishalAbdullah <85678545+Mishalabdullah@users.noreply.github.com>
2023-06-03 12:46:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  target: 'experimental-serverless-trace',
+const nextConfig = {
   experimental: {
     appDir: true,
   },
 };
-
 
 module.exports = nextConfig;
