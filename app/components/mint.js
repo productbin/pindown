0c04cef70023b9f95ea2f6623ee939bf68a17938
Mishalabdullah <github.ijvob@simplelogin.co>
2023-06-03 11:14:25 +0530

    mint button to another file

---
+++ app/components/mint.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { useContractWrite } from "wagmi";
+import abiData from "../upload/abi.json";
+import { ipfsURL, theData } from "../upload/page";
+import Web3AuthConnectorInstance from "../components/web3config";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: "TVxT2Kjzsy4pFTaoWknc3O8SwxAuUqm6" }),
+    publicProvider(),
+  ]
+);
+
+export default function Mint() {
+  const config = {
+    address: "0x41E405438dF59D438D62385e762B7e4B54AE2517",
+    abi: abiData,
+    functionName: "bulkMint",
+    args: [ipfsURL, theData],
+    onSuccess(data) {
+      alert("Transaction Successful");
+    },
+    isError(data) {
+      console.log("Problems");
+    },
+  };
+
+  const { write } = useContractWrite(config);
+  return (
+    <div className="flex justify-center">
+      <button onClick={write} className="relative inline-block text-lg group">
+        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+          <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+          <span className="absolute left-0 w-64 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  group-hover:-rotate-180 ease"></span>
+          <span className="relative">Mint NFT</span>
+        </span>
+        <span
+          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  rounded-lg group-hover:mb-0 group-hover:mr-0"
+          data-rounded="rounded-lg"
+        ></span>
+      </button>{" "}
+    </div>
+  );
+}
--- app/upload/page.js
+++ app/upload/page.js
@@ -25,13 +25,13 @@ import { isAddress } from "web3-utils";
 import Web3AuthConnectorInstance from "../components/web3config";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import Web3AuthProfile from "../components/web3profile";
-
+import Mint from "../components/mint";
 library.add(faInfoCircle);
 // Global Vaiables
 
-var ipfsURL;
+export var ipfsURL;
 var jsonList = [];
-var theData = [];
+export var theData = [];
 var theImageUrl;
 const { chains, provider, webSocketProvider } = configureChains(
   [polygonMumbai],
@@ -50,37 +50,6 @@ const client = createClient({
   provider,
   webSocketProvider,
 });
-function Mint() {
-  const config = {
-    address: "0x41E405438dF59D438D62385e762B7e4B54AE2517",
-    abi: abiData,
-    functionName: "bulkMint",
-    args: [ipfsURL, theData],
-    onSuccess(data) {
-      alert("Transaction Successful");
-    },
-    isError(data) {
-      console.log("Problems");
-    },
-  };
-
-  const { write } = useContractWrite(config);
-  return (
-    <div className="flex justify-center">
-      <button onClick={write} className="relative inline-block text-lg group">
-        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-          <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-          <span className="absolute left-0 w-64 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  group-hover:-rotate-180 ease"></span>
-          <span className="relative">Transfer The NFT's</span>
-        </span>
-        <span
-          className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear hover:from-purple-600 hover:to-pink-600 ease bg-gradient-to-br from-purple-500 to-pink-500  rounded-lg group-hover:mb-0 group-hover:mr-0"
-          data-rounded="rounded-lg"
-        ></span>
-      </button>{" "}
-    </div>
-  );
-}
 
 export default function Upload() {
   const [uploading, setUploading] = useState(false);
